07ce19cddf88007137a8da416540defeef067a4e
Gabriela Borges <borges676@gmail.com>
2023-05-12 22:18:55 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+function getProjects (){
+    const urlGitHub ='https://api.github.com/users/borgesgabriela/repos'
+    var loadingElement =document.getElementById('loading')
+
+fetch(urlGitHub,{
+    method: 'GET'
+
+    })
+        .then((response) => response.json())
+        .then((response) => {
+            loadingElement.style.display = 'none'
+            showProjects(response)
+        })
+        .catch((e) =>{
+            console.log(e)
+        })
+}
+
+function showProjects(data){
+    var listElement = document.getElementById('my-projects-list')
+
+    for(let i = 0; i < data.length; i++)
+    {
+        let a = document.createElement("a")
+        a.href = data [i] ['clone_url']
+        a.target ='_blank'
+        a.title = data [i]['description']
+        let linkText = document.createTextNode(data [i]['name'])
+        a.appendChild(linkText)
+        listElement.appendChild(a)
+    }   
+}
+getProjects()
